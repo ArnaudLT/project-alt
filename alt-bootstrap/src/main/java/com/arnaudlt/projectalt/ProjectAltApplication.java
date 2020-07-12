7d74fce392dc600f982cfc2e8f277850fc5178dc
Arnaud <arn.letort@gmail.com>
2020-07-12 14:33:46 +0200

    Add spring-boot-maven-plugin

--- alt-bootstrap/src/main/java/com/arnaudlt/projectalt/ProjectAltApplication.java
+++ alt-bootstrap/src/main/java/com/arnaudlt/projectalt/ProjectAltApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages= {"com.arnaudlt.projectalt"})
+@SpringBootApplication
 public class ProjectAltApplication {
 
 	public static void main(String[] args) {
